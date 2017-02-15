c74c993753c990d7862adbbbfdc6812dc8da66e3
cwilson04 <cassiewilson4@gmail.com>
2017-02-15 13:26:46 +0000

    added  to AlbumCtrl

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,13 @@
-(function() {
-     function AlbumCtrl() {
-        //Add an albumData property that holds a copy of albumPicasso.
-        this.albumData = angular.copy(albumPicasso);
+ (function() {
+     function AlbumCtrl($scope) {
+         $scope.song = 'this is a song name';
+         $scope.number = 'num';
+         $scope.duration = '3.00';
+         this.albumData = angular.copy(albumPicasso);
      }
  
      angular
          .module('blocJams')
          .controller('AlbumCtrl', AlbumCtrl);
  })();
+ 
