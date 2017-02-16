5873f910e125ad60fa60588a322ccd56b2cd9b46
cwilson04 <cassiewilson4@gmail.com>
2017-02-16 22:13:53 +0000

    Collection page still blank after updating Fixtures - getCollection function

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -35,8 +35,14 @@
         
         Fixtures.getAlbum = function() {
          return albumPicasso;
-     };
+        };
  
+        Fixtures.getCollection = function() {
+            this.albums = [];
+            for (var i=0; i < 12; i++) {
+                this.albums.push(angular.copy(albumPicasso));
+            }
+        };
  
          return Fixtures;
      }
